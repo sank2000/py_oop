54dda9e716b1f2ce936cd221db4d117a5d1757d7
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-06-14 18:12:18 +0530

    a few updates

--- class vaiables.py
+++ class vaiables.py
@@ -7,9 +7,16 @@ class Student:
     def display(self):
         print("\nName   :",self.name)
         print("Roll no:",self.rollno)
-        print("Clg    :",Student.clg)
+        print("Clg    :",self.clg)
 
 s1 = Student("12345","san")
 s1.display()
 s2 = Student("12346","vel")
 s2.display()
+
+try:
+    s2.clg = 'AUBIT'
+    print('clg: ', s2.clg)
+
+except Exception as e:
+    print('Error occured!', e)
