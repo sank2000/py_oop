a319dc47439b74ba191ad225faa8f90f59b520b2
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-06-14 17:52:20 +0530

    workaround for obj variables conflict

--- simple_class.py
+++ simple_class.py
@@ -12,5 +12,12 @@ obj = MyClass()
 obj.something = 1
 obj.what_the_hell = 'test string'
 
+obj2 = MyClass()
+
 print('something is ', obj.something)
 print('what_the_hell is ', obj.what_the_hell)
+
+try:
+    print('something from obj2: ', obj2.something)
+except Exception as e:
+    print('not a class variable', e)
